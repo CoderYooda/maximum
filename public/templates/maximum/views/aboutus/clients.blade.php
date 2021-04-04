15173dd651a6946c417bbdb9fad6f54754a3aff3
CoderYooda <CoderYooda@gmail.com>
2021-04-04 12:17:20 +0300

    awd

--- public/templates/maximum/views/aboutus/clients.blade.php
+++ public/templates/maximum/views/aboutus/clients.blade.php
@@ -1,14 +1,14 @@
 <div class="row">
-    @foreach($clients as $client)
         <div class="col-lg-12 mb-30">
             <div class="row">
+                @foreach($clients as $client)
                 <div class="col-lg-4">
                     <div class="white_cell">
                         <img class="client_img" src="{{ count($client->images) ? $client->images[0]->url : '/images/noimage.jpg' }}" alt="">
                         <div class="name_thin">{{ $client->name }}</div>
                     </div>
                 </div>
+                @endforeach
             </div>
         </div>
-    @endforeach
 </div>
